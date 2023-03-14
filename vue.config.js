b33252f48a15f9502a6ac9083bb1e8bb758fc75a
WillRoDias <willrodias@gmail.com>
2023-03-14 15:42:03 -0300

    removing dist subtree

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,6 @@
 const path = require("path");
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/teste_descoberta/" : "/",
   pluginOptions: {
     "style-resources-loader": {
       preProcessor: "scss",
